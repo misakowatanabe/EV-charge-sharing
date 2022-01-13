7502b2a924b8a2780a5983143dfb7a702e3e8bbf
Misako Watanabe <misakolani@gmail.com>
2022-01-13 14:59:32 +0100

    added skeleton to simple profile on app bar

--- src/layout/SimpleProfile.tsx
+++ src/layout/SimpleProfile.tsx
@@ -1,12 +1,41 @@
+import { useState, useEffect } from "react";
 import { useAppSelector } from "../context/Hooks";
 import { selectProfileData } from "../context/slices/ProfileDataSlice";
 import Avatar from "@mui/material/Avatar";
 import PersonIcon from "@mui/icons-material/Person";
 import Typography from "@mui/material/Typography";
+import Skeleton from "@mui/material/Skeleton";
 
 export default function SimpleProfile() {
   const profile = useAppSelector(selectProfileData);
-  var name = profile.name;
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    if (profile) {
+      setName(profile.name);
+    }
+  }, [profile]);
+
+  var displayName;
+  var icon;
+  if (name === "") {
+    displayName = <Skeleton height={30} width="100px" />;
+    icon = <Skeleton variant="circular" width={40} height={40} />;
+  } else {
+    displayName = name;
+    icon = (
+      <Avatar
+        alt="Avatar"
+        sx={{
+          width: 40,
+          height: 40,
+          margin: { xs: "0 0 0 auto", sm: "0" },
+        }}
+      >
+        <PersonIcon style={{ fontSize: "30px" }} />
+      </Avatar>
+    );
+  }
 
   return (
     <>
@@ -20,18 +49,9 @@ export default function SimpleProfile() {
           display: { xs: "none", sm: "block" },
         }}
       >
-        {name}
+        {displayName}
       </Typography>
-      <Avatar
-        alt="Avatar"
-        sx={{
-          width: 40,
-          height: 40,
-          margin: { xs: "0 0 0 auto", sm: "0" },
-        }}
-      >
-        <PersonIcon style={{ fontSize: "30px" }} />
-      </Avatar>
+      {icon}
     </>
   );
 }
